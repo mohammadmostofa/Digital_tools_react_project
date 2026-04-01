ab35cfe9d48fed3ddab751348e75f3954dab9a0e
mohammadmostofa <mohammadmostofa.dev@gmail.com>
2026-04-02 00:06:54 +0600

    added in repo name

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/https://github.com/mohammadmostofa/Digital_tools_react_project/',
   plugins: [
     react(),
    tailwindcss(),
