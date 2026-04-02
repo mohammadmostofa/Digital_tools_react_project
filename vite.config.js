a0715d4a2ff1e031a7eb43ed8e846087253e8cf8
mohammadmostofa <mohammadmostofa.dev@gmail.com>
2026-04-02 22:10:12 +0600

    added fix and solve issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Digital_tools_react_project/', // এই লাইনটি ৪-০-৪ এরর দূর করবে
 })
